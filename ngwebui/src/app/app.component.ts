e98fbab36b8a362f585ba1540071bcb08f40264a
Virgilio Dolosa <vdolosa@live.com.ph>
2024-08-22 14:22:54 +0800

    Initial implementation of SignalR

--- ngwebui/src/app/app.component.ts
+++ ngwebui/src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'
+import { AppSignalRService } from './app-signalr.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngwebui';
+  receivedMessage: string = "";
+  textBoxValue: string = '';
+
+  constructor(private signalRService: AppSignalRService) { }
+
+  ngOnInit(): void {
+    this.signalRService.startConnection().subscribe(() => {
+      this.signalRService.receiveMessage().subscribe((message) => {
+        this.receivedMessage = message;
+      });
+    });
+  }
+
+  logTextBoxValue() {
+    //console.log('Textbox value:', this.textBoxValue);
+    this.sendMessage(this.textBoxValue);
+  }
+
+  sendMessage(message: string): void {
+    this.signalRService.sendMessage(message);
+  }
 }
